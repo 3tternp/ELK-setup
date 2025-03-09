cb46ca1e1b82d41e0c0a18838199b17925c24980
astra <26215963+3tternp@users.noreply.github.com>
2025-03-09 15:40:23 +0545

    Add files via upload

--- ELK.sh
+++ ELK.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 set -e  # Exit immediately if a command exits with a non-zero status
-
 # Update system packages
 sudo apt update -y
-
 # Add Elastic Repository
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
